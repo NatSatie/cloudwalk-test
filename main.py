fa760d77b48d5cc07c308a9fd56b49cffa88f8bc
Natália Satie <naty.odashima@gmail.com>
2022-10-21 17:19:45 -0300

    refactor: move functions

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import argparse
+from asyncore import write
 from ReadFile import matchRegex
 import sys
 
-if __name__ == "__main__":
+def argumentParser():
 	parser = argparse.ArgumentParser(
 		description='Parse log file of quake game matches')
-	parser.add_argument('-f', '--logfile',
+	parser.add_argument('-i', '--input',
 						dest='logfile',
 						metavar='logfile',
 						action='store',
-						help='TODO: add help description',
+						help='Read the file path of the Quake Game Log.',
 						required=True)
+	parser.add_argument('-o', '--output',
+						dest='output',
+						metavar='output',
+						action='store',
+						help='Write the output file path of the Quake Game Log.',
+						required=True)
+	return parser.parse_args()
 
-	args = parser.parse_args()
+def writeOutputFile(outputFile, gameResult):
+	outputFile = args.output
+	originalStdout = sys.stdout
+	with open(outputFile, 'w') as f:
+		sys.stdout = f
+		print(gameResult)
+		sys.stdout = originalStdout
+
+if __name__ == "__main__":
+	args = argumentParser()
 	print(args)
 	if args:
-		gameMatches = matchRegex(args.logfile)
-		originalStdout = sys.stdout
-		with open('gameListOutcome.txt', 'w') as f:
-			sys.stdout = f
-			print(gameMatches)
-			sys.stdout = originalStdout
+		gameResult = matchRegex(args.logfile)
+		outputFile = args.output
+		writeOutputFile(outputFile, gameResult)
